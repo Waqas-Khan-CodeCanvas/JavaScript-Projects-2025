5ab269f1e4217f70f9f2027cee00ecb36ea28a85
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-02-22 18:41:28 +0500

    10 Sticky navbar added

---
+++ 10-sticky-navbar/sticky-navbar.js
@@ -0,0 +1,20 @@
+const navElement = document.querySelector("nav")
+const navbarLinks = navElement.querySelectorAll("a")
+
+const navPosition = navElement.getBoundingClientRect().top;
+
+window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY;
+    navElement.style.top = scrollPosition + "px";
+
+    navbarLinks.forEach(link => {
+        const sectionElement= document.querySelector(link.hash);
+        const offset=60;
+        if (scrollPosition +offset>sectionElement.offsetTop && scrollPosition +offset < sectionElement.offsetTop+sectionElement.offsetHeight){
+            link.classList.add("active");
+        }else{
+            link.classList.remove("active");
+        }
+
+    });
+});
