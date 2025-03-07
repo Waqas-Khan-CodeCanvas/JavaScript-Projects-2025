3a5b90e2c1c06fabccc0146db7cf05770bf39bd6
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-08 00:15:23 +0500

    toast notifier

---
+++ 20-toastnitification/toast.js
@@ -0,0 +1,23 @@
+const button = document.querySelector("button"),
+    toast = document.querySelector(".toast"),
+    closeIcon = document.querySelector(".close"),
+    progress = document.querySelector(".progress");
+
+button.addEventListener("click", () => {
+    toast.classList.add("active");
+    progress.classList.add("active");
+
+    setTimeout(() => {
+        toast.classList.remove("active");
+    }, 5000);
+    setTimeout(() => {
+        progress.classList.remove("active");
+    }, 5500);
+})
+closeIcon.addEventListener("click", () => {
+    toast.classList.remove("active");
+
+    setTimeout(() => {
+        toast.classList.remove("active");
+    }, 300);
+})
